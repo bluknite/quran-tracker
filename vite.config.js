e26d10a8b455d77419e3c1ab931f55c02821a39d
Siraj Ahmed <blu.knite@gmail.com>
2026-02-20 16:03:44 -0800

    Fix Vite base path and image URLs for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Quran/', // Set base for GitHub Pages
+  base: '/quran-tracker/', // Set base for GitHub Pages
 })
